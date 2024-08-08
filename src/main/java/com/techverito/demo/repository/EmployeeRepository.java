70f3ac1a454f6ee930b53e9e45eb4dba9a9f0bc9
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-08-08 23:24:02 +0530

    Add findByEmail method in EmployeeRepository

---
+++ src/main/java/com/techverito/demo/repository/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.techverito.demo.repository;
+
+import com.techverito.demo.dto.EmployeeRequestBody;
+import com.techverito.demo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Optional<Employee> findByEmail(String email);
+
+
+}

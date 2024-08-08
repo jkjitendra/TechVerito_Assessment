707550c69472784507b4d30175ba4f851fb7f336
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-08-08 23:23:38 +0530

    Add findByName method in DepartmentRepository

---
+++ src/main/java/com/techverito/demo/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package com.techverito.demo.repository;
+
+import com.techverito.demo.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Optional<Department> findByName(String name);
+}

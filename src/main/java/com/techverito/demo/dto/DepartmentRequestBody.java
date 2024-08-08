4e85a7af24c9416d27430f249694420384713c16
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-08-08 23:24:47 +0530

    Create DepartmentRequestBody DTO

---
+++ src/main/java/com/techverito/demo/dto/DepartmentRequestBody.java
@@ -0,0 +1,15 @@
+package com.techverito.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepartmentRequestBody {
+
+    private String name;
+}

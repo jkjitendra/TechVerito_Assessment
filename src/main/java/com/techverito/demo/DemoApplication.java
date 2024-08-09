e3b6f69b9e0506a4ba076280fba9f82ff8965692
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-08-10 00:21:21 +0530

    entry point to spring application

---
+++ src/main/java/com/techverito/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.techverito.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+// Employee name, salary Department department
